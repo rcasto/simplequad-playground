fc73e05d9c68af1c0bdeab95d23cedd8dcb6f4de
Richie Casto <rcasto92@gmail.com>
2019-07-21 22:49:53 -0700

    more setup, drawing multiple particles moving

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ interface Particle extends CollisionObject, Circle {
     v: Vector;
     lastUpdate: number;
     update: (timestamp: number) => void;
-    draw: (context: CanvasRenderingContext2D) => void;
+    draw: (context: CanvasRenderingContext2D, isCollided: boolean) => void;
 }
 
 const capacity: number = 10;
@@ -22,8 +22,15 @@ const bounds: BoundingBox = {
 const canvas: HTMLCanvasElement = document.getElementById('canvas') as unknown as HTMLCanvasElement;
 const context: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
 const quadTree: QuadTree = createQuadTree(bounds, capacity);
-const particle: Particle = createParticle();
-const dampenFactor: number = 0.9;
+const wallDampenFactor: number = 0.8;
+const particleRadius: number = 5;
+const particleVelocity: Vector = {
+    x: 1,
+    y: 1,
+};
+const TWO_PI: number = 2 * Math.PI;
+const maxParticlesInWorld: number = 100;
+const particlesInWorld: Particle[] = [];
 
 canvas.width = bounds.width;
 canvas.height = bounds.height;
@@ -31,55 +38,80 @@ canvas.height = bounds.height;
 function animate(animateTimestamp: number = 0) {
     window.requestAnimationFrame(animate);
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    particle.update(animateTimestamp);
-    quadTree.add(particle);
-
-    const worldObjects: Set<CollisionObject> = quadTree.query(quadTree.bounds);
-
-    // If there are know objects within the overall bounds
-    // It means the particle is out of the world bounds
-    if (worldObjects.size <= 0) {
-        particle.x = Math.max(Math.min(particle.x + particle.r, quadTree.bounds.x + quadTree.bounds.width), quadTree.bounds.x + particle.r);
-        particle.y = Math.max(Math.min(particle.y + particle.r, quadTree.bounds.y + quadTree.bounds.height), quadTree.bounds.y + particle.r);
-
-        // goes out of bounds on the x axis
-        if (particle.x === quadTree.bounds.x + quadTree.bounds.width ||
-            particle.x === quadTree.bounds.x + particle.r) {
-            particle.v.x *= -1 * dampenFactor;
-        }
-
-        // goes out of bounds on the y axis
-        if (particle.y === quadTree.bounds.y + quadTree.bounds.height ||
-            particle.y === quadTree.bounds.y + particle.r) {
-            particle.v.y *= -1 * dampenFactor;
-        }
+    // Check if we are at capacity with particles
+    if (particlesInWorld.length + 1 > maxParticlesInWorld) {
+        // clear held particles
+        particlesInWorld.length = 0;
+    } else {
+        // add a new particle
+        const particle: Particle = createParticle(quadTree.bounds);
+        particlesInWorld.push(particle);
     }
 
-    particle.draw(context);
+    // Clear the canvas and the quadtree
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    quadTree.clear();
+
+    // Build quadtree for particles in world
+    particlesInWorld
+        .forEach(particleInWorld => quadTree.add(particleInWorld));
+
+    // Update the state of each particle (move it)
+    particlesInWorld
+        .forEach(particleInWorld => particleInWorld.update(animateTimestamp));
+
+    // Resolve the state of each particle such that it isn't out of the world bounds
+    particlesInWorld
+        .forEach(particleInWorld => resolveParticleWorldCollisions(particleInWorld, quadTree));
+
+    // Draw each particle in the world
+    particlesInWorld
+        .forEach(particleInWorld => {
+            const particleWindowQueryResultSet: Set<CollisionObject> = quadTree.query(particleInWorld.getBounds());
+            particleInWorld.draw(context, particleWindowQueryResultSet.size > 1);
+        });
 }
 
 // Resolves all particle collisions
-function resolveParticleCollisions(particle: Particle, world: QuadTree) {
-    resolveParticleWorldCollision(particle, quadTree);
-}
+function resolveParticleWorldCollisions(particle: Particle, world: QuadTree): void {
+    const minX: number = quadTree.bounds.x + particle.r;
+    const minY: number = quadTree.bounds.y + particle.r;
+    const maxX: number = quadTree.bounds.x + quadTree.bounds.width - particle.r;
+    const maxY: number = quadTree.bounds.y + quadTree.bounds.height - particle.r;
 
-function resolveParticleWorldCollision(particle: Particle, world: QuadTree) {
-
+    // Outside at the left of the canvas
+    if (particle.x <= minX) {
+        particle.x = minX;
+        particle.v.x *= -1 * wallDampenFactor;
+    }
+    // Outisde at the right of the canvas
+    else if (particle.x >= maxX) {
+        particle.x = maxX;
+        particle.v.x *= -1 * wallDampenFactor;
+    }
+    // Outside at the top of the canvas
+    else if (particle.y <= minY) {
+        particle.y = minY;
+        particle.v.y *= -1 * wallDampenFactor;
+    }
+    // Outside at the bottom of the canvas
+    else if (particle.y >= maxY) {
+        particle.y = maxY;
+        particle.v.y *= -1 * wallDampenFactor;
+    }
 }
 
 // function createStaticParticle()
 // function createDynamicParticle()
-
-function createParticle(): Particle {
+function createParticle(bounds: BoundingBox): Particle {
+    const x = (bounds.x + bounds.width - particleRadius) * Math.random() + bounds.x + particleRadius;
+    const y = (bounds.y + bounds.height - particleRadius) * Math.random() + bounds.y + particleRadius;
     return {
-        x: 0, 
-        y: 0,
-        r: 5,
+        x, 
+        y,
+        r: particleRadius,
         v: {
-            x: 1,
-            y: 1,
+            ...particleVelocity,
         },
         lastUpdate: 0,
         update(timestamp: number) {
@@ -88,10 +120,19 @@ function createParticle(): Particle {
             this.x += this.v.x * deltaTimeInMs;
             this.y += this.v.y * deltaTimeInMs;
         },
-        draw(context) {
+        draw(context, isCollided) {
             context.beginPath();
-            context.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+            context.strokeStyle = 'black';
+            context.fillStyle = 'blue';
+            context.arc(this.x, this.y, this.r, 0, TWO_PI);
             context.stroke();
+            if (isCollided) {
+                context.fill();
+            }
+            context.beginPath();
+            context.fillStyle = 'red';
+            context.arc(this.x, this.y, 1, 0, TWO_PI);
+            context.fill();
             context.closePath();
         },
         getBounds() {
@@ -104,6 +145,7 @@ function createParticle(): Particle {
     };
 }
 
+// start the animation
 if (context) {
     animate();
 }
